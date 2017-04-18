e3ddeb0f9c42e302c863f7dd19938398a912eee1
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-04-18 22:56:29 +0200

    Update comments

--- simulation.m
+++ simulation.m
@@ -5,20 +5,22 @@ signature1 = crc32(image);
 
 disp(signature1)
 
+%If repeat is set to 1, message needs to be repeated
 repeat = 1;
 count = 1;
 chanceToFail = 0.8;
 while repeat
     %Simulate sending message through channel with 80% chance to change the
-    %message. Display how many attempts took to send the message properly.
+    %message. Display the current received message signature and how many 
+    %attempts took to send the message properly.
     disp(count)
     count = count + 1;
     %Message after passing through communication chanel
     recived = sendData(image,chanceToFail);
-    %compute new signature
+    %Compute new signature
     signature2 = crc32(recived);
     disp(signature2)
-    %repeat if needed
+    %Repeat if needed
     repeat = compareSignatures(signature1,signature2);     
 end  
 
